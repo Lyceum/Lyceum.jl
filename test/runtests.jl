2aca4df55bb65378fcde1d14e8018e009651a799
colinxs <me@colinxsummers.com>
2020-05-11 19:39:37 -0700

    rm tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,5 @@
-using Test, Pkg, Lyceum
-
-function test_package(pkg::String)
-    try
-        Pkg.test(pkg)
-        return true
-    catch
-        return false
-    end
-end
+using Test
 
 @testset "Lyceum.jl" begin
-    @testset "$pkg" for pkg in [Lyceum.LYCEUM_PACKAGES..., Lyceum.UNUSED_LYCEUM_PACKAGES...]
-        @test test_package(pkg)
-    end
+    @test true
 end
